006edd619b1c28e715670ca5bf3e5f8495ecdfdf
Ali Zaib <alizaib2706@gmail.com>
2021-04-14 22:24:42 +0500

    first commit

---
+++ myscript.js
@@ -0,0 +1,63 @@
+
+const selectArea = document.querySelectorAll(".select-content"); //selects all the elements within that specific class                                                  
+const theButton = document.querySelector(".button"); // selects the button element which is hidden by default
+const dialogBox = document.querySelector(".dialog-box") ; // selects the textarea which is hidden by default                        
+
+
+// for each child element within the select-content class add a mouseup event listener
+selectArea.forEach((element) => {
+        
+        if(element.tagName !== "BUTTON" || element.tagName !== "TEXTAREA" ){
+            element.addEventListener("mouseup", loggerFunction , false);
+        }
+       
+});
+
+
+// this function handles the functionality on mouseup event
+function loggerFunction(e) {
+    // console.log(e.target.className) ;
+    setTimeout(() => {  //settimeout set to zero so the selected text is deselected if the user again clicks on it
+
+      const text = window.getSelection().toString().trim(); //get the selected text using the getSelection method
+    // console.log(text) ;
+      
+        if(text.length > 0){
+          theButton.style.display = "block" ;
+        
+        const x = e.clientX ; //x coordinates of the pointer where the mouseup event was triggered
+        const y = e.clientY ; // y coordinates of the pointer where the mouseup event was triggered
+          
+    
+          theButton.style.left = `${x-20}px` ;
+          theButton.style.top = `${y+20}px` ;
+    
+      }
+    },0)
+  
+}
+
+
+ document.addEventListener("mousedown" , buttonHider); 
+
+
+function buttonHider(e){     // event handler for mouse down event
+    // console.log(e.target.nodeName) ;
+     if(getComputedStyle(theButton).display === "block" && e.target.nodeName !=="BUTTON" 
+        && e.target.nodeName !== "TEXTAREA"){
+        theButton.style.display = "none" ;
+        dialogBox.style.display = "none" ;
+        window.getSelection().empty ;
+    }
+}
+
+theButton.addEventListener("click" , thebuttonClick) ;
+
+function thebuttonClick(e){
+   
+    dialogBox.style.display = "block" ;
+    const x = e.clientX ;
+    const y = e.clientY ;
+    dialogBox.style.left = `${x - 20}px` ;
+    dialogBox.style.top = `${y+25}px` ;
+}
